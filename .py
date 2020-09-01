bba7ccc3595a30a67601be1523d24b63b219827d
MADAVARAM LAYA <madavaramlaya@gmail.com>
2020-09-01 20:43:09 +0530

    Update .py

--- .py
+++ .py
@@ -1,8 +1,12 @@
 from random import randint
 from math import sqrt
+# Enter the maximum limit.
 n=int(input("Set the maximum value, you wanted to guess the number : "))
+# Generating the number between 1 and your specified maximum value.
 a=randint(1,n)
+# Counting the number of chances.
 count=0
+# Actions displaying after each response given by you.
 while 1:
     count+=1
     if(count<=sqrt(n)):
@@ -18,3 +22,4 @@ while 1:
     else:
         print("\nOops...! You lose the game.")
         break
+# Guess the number within the number(square root of given maximum limit) of chances. 
