f502ee1b32c39ba8d1a5acbc3579e801fe10e5ad
MADAVARAM LAYA <madavaramlaya@gmail.com>
2020-09-02 07:08:14 +0530

    Update .py

--- .py
+++ .py
@@ -5,11 +5,13 @@ n=int(input("Set the maximum value, you wanted to guess the number : "))
 # Generating the number between 1 and your specified maximum value.
 a=randint(1,n)
 # Counting the number of chances.
+N_of_chances=int(sqrt(n))
+print("\nYou will get",N_of_chances,"chances to guess the number.")
 count=0
 # Actions displaying after each response given by you.
 while 1:
     count+=1
-    if(count<=sqrt(n)):
+    if(count<=N_of_chances):
         guess=int(input("\nEnter the number you guessed : "))
         if(guess<a):
             print("\nToo low. Try again...")
@@ -18,7 +20,7 @@ while 1:
         else:
             print("\nCongratulations...! You won.")
             break
-        print("\nChances remaining : ",int(sqrt(n)-count))
+        print("\nChances remaining : ",N_of_chances-count))
     else:
         print("\nOops...! You lose the game.")
         break
